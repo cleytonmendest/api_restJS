7626fb6df884d6f89606ac5c19d29fa595223f9a
Cleyton Mendes <cleyton.mendest@gmail.com>
2021-07-11 14:37:24 -0300

    Correção de bugs e tratamento de arquivos enviado para somente arquivos de imagem

--- src/routes/fotoRouters.js
+++ src/routes/fotoRouters.js
@@ -1,11 +1,9 @@
 import { Router } from 'express';
-import multer from 'multer';
-import FotoController from '../controllers/FotoController';
-import multerConfig from '../config/multerConfig';
 
-const upload = multer(multerConfig);
+import FotoController from '../controllers/FotoController';
+
 const router = new Router();
 
-router.post('/', upload.single('foto'), FotoController.store);
+router.post('/', FotoController.store);
 
 export default router;
